54f9a37b7bc3460a5c791d62b3f265d214d1273f
nnelluri928 <nnelluri928@gmail.com>
2021-07-21 22:56:18 -0700

    seacrh for a file

---
+++ search_file.py
@@ -0,0 +1,14 @@
+import os
+import argparse
+
+my_parser = argparse.ArgumentParser(description="reading directory path to find the file")
+my_parser.add_argument("pathname",
+                        help="please enter the directory path")
+my_parser.add_argument("filesearch",
+                        help="Please enter the filename to search")
+args = my_parser.parse_args()
+
+for dirname,dirpath,filename in os.walk(args.pathname):
+    for file in filename:
+        if file == args.filesearch:
+            print(os.path.join(dirname,file))
